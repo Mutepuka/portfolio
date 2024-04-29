7f76fa687d963866c57863ab7e4864871671bf5c
Musonda Makena <Musondamakena@gmail.com>
2024-04-29 16:41:10 +0200

    page jsx full

--- app/page.jsx
+++ app/page.jsx
@@ -8,11 +8,11 @@ import Header from '@components/Header';
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
-      {/* <Header/>
+      <Header/>
         <About/>
         <Work/>
         <Skills/>
-        <Testimonials/> */}
+        <Testimonials/>
     </section>
   )
 }
